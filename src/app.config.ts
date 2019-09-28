7fbb7cc12caec3b67204a371eb4efe3a7dc76a41
sun <asmh1989@gmail.com>
2019-09-28 21:22:47 +0800

    init by nodepress

---
+++ src/app.config.ts
@@ -0,0 +1,26 @@
+/**
+ * App config.
+ * @file 应用运行配置
+ */
+
+import { argv } from 'yargs';
+
+export const APP = {
+  LIMIT: 16,
+  PORT: 3000,
+  ROOT_PATH: __dirname,
+};
+
+export const MONGODB = {
+  uri: `mongodb://127.0.0.1:${argv.dbport || '27017'}/bg`,
+  username: argv.db_username || 'DB_username',
+  password: argv.db_password || 'DB_password',
+};
+
+export const QINIU = {
+  accessKey: argv.qn_accessKey || 'qiniu access key',
+  secretKey: argv.qn_secretKey || 'qiniu secret key',
+  bucket: argv.qn_bucket || 'qiniu bucket name',
+  origin: argv.qn_origin || 'qiniu origin url',
+  uploadURL: argv.qn_uploadURL || 'http://up.qiniu.com/',
+};
---
+++ src/main.ts
@@ -0,0 +1,53 @@
+import * as APP_CONFIG from '@app/app.config';
+import * as helmet from 'helmet';
+import * as bodyParser from 'body-parser';
+import * as compression from 'compression';
+import * as rateLimit from 'express-rate-limit';
+import { AppModule } from '@app/app.module';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ValidationPipe } from '@app/pipes/validation.pipe';
+import { HttpExceptionFilter } from '@app/filters/error.filter';
+import { TransformInterceptor } from '@app/interceptors/transform.interceptor';
+import { LoggingInterceptor } from '@app/interceptors/logging.interceptor';
+import { ErrorInterceptor } from '@app/interceptors/error.interceptor';
+import { environment, isProdMode, isDevMode } from '@app/app.environment';
+
+// 解决 Nodejs 环境中请求 HTTPS 的证书授信问题
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+// 替换 console 为更统一友好的
+const { log, warn, info } = console;
+const color = (c: string) => (isDevMode ? c : '');
+Object.assign(global.console, {
+  log: (...args: any) => log('[log]', ...args),
+  warn: (...args: any) =>
+    warn(color('\x1b[33m%s\x1b[0m'), '[warn]', '[bp]', ...args),
+  info: (...args: any) =>
+    info(color('\x1b[34m%s\x1b[0m'), '[info]', '[bp]', ...args),
+  error: (...args: any) =>
+    info(color('\x1b[31m%s\x1b[0m'), '[error]', '[bp]', ...args),
+});
+
+async function bootstrap() {
+  const app = await NestFactory.create(
+    AppModule,
+    isProdMode ? { logger: false } : null,
+  );
+  app.use(helmet());
+  app.use(compression());
+  app.use(bodyParser.json({ limit: '1mb' }));
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(rateLimit({ max: 1000, windowMs: 15 * 60 * 1000 }));
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(
+    new TransformInterceptor(new Reflector()),
+    new ErrorInterceptor(new Reflector()),
+    new LoggingInterceptor(),
+  );
+  return await app.listen(APP_CONFIG.APP.PORT);
+}
+
+bootstrap().then(_ => {
+  console.info(`Bp Run！port at ${APP_CONFIG.APP.PORT}, env: ${environment}`);
+});
